517f080f4da0e90930bc3a5791f51931511109d9
Sami Kyostila <skyostil@google.com>
2017-10-30 18:05:36 +0000

    [Patchset 1] DO NOT SUBMIT: Hacking around with grpc
    Change-Id: I473c3eaf36091dfb0f805783844aa439b1ab3966

---
+++ buildtools/grpc_test.cc
@@ -0,0 +1,82 @@
+#include <grpc/grpc.h>
+#include <grpc++/server.h>
+#include <grpc++/server_builder.h>
+#include <grpc++/server_context.h>
+#include <grpc++/security/server_credentials.h>
+#include <grpc++/grpc++.h>
+
+#include "helloworld.grpc.pb.h"
+
+using grpc::Server;
+using grpc::ServerBuilder;
+using grpc::ServerContext;
+using grpc::Status;
+
+using grpc::Channel;
+using grpc::ChannelInterface;
+using grpc::ClientContext;
+using grpc::Status;
+
+class GreeterServiceImpl final : public Greeter::Service {
+  Status SayHello(ServerContext* context, const HelloRequest* request,
+                  HelloReply* reply) override {
+    std::string prefix("Hello ");
+    reply->set_message(prefix + request->name());
+    return Status::OK;
+  }
+};
+
+class GreeterClient {
+ public:
+  GreeterClient(std::shared_ptr<ChannelInterface> channel)
+      : stub_(Greeter::NewStub(channel)) {}
+
+  // Assembles the client's payload, sends it and presents the response back
+  // from the server.
+  std::string SayHello(const std::string& user) {
+    // Data we are sending to the server.
+    HelloRequest request;
+    request.set_name(user);
+
+    // Container for the data we expect from the server.
+    HelloReply reply;
+
+    // Context for the client. It could be used to convey extra information to
+    // the server and/or tweak certain RPC behaviors.
+    ClientContext context;
+
+    // The actual RPC.
+    Status status = stub_->SayHello(&context, request, &reply);
+
+    // Act upon its status.
+    if (status.ok()) {
+      return reply.message();
+    } else {
+      return "RPC failed";
+    }
+  }
+
+ private:
+  std::unique_ptr<Greeter::Stub> stub_;
+};
+
+int main(int argc, char**) {
+  if (argc > 1) {
+    GreeterClient greeter(grpc::CreateChannel(
+    "localhost:50051", grpc::InsecureChannelCredentials()));
+    std::string user("world");
+    std::string reply = greeter.SayHello(user);
+    printf("Greeter received: %s\n", reply.c_str());
+    return 0;
+  }
+
+  std::string server_address("0.0.0.0:50051");
+  GreeterServiceImpl service;
+
+  ServerBuilder builder;
+  builder.AddListeningPort(server_address, grpc::InsecureServerCredentials());
+  builder.RegisterService(&service);
+  std::unique_ptr<Server> server(builder.BuildAndStart());
+
+  server->Wait();
+}
